5cc11e24dce205d8a1692c37cf82f04e4de689a2
Trenton Smith <trenton.smith@workiva.com>
2017-03-15 15:21:45 -0600

    update example

--- banzai/examples/npc_ex.py
+++ banzai/examples/npc_ex.py
@@ -1,6 +1,10 @@
 import banzai
 from banzai.api import npcs
 
+banzai.init()
+
 bankers = npcs.get_all(name_id='Banker')
 for banker in bankers:
     print(banker.get_position())
+
+banzai.terminate()
